f98292c5e3fa9369ace80893e713738af0e83542
pp0825pp <905092043@qq.com>
2013-12-15 20:37:09 +0800

    Update 2.1-插入排序.cpp

--- 2.1-插入排序.cpp
+++ 2.1-插入排序.cpp
@@ -6,11 +6,11 @@ int main()
     int A[6] = {8, 2, 4, 9, 3, 6};
     for(j = 1; j <= 5; j++)
     {
-    	key = A[j];
-    	i = j - 1;
-  		while(i >= 0 && A[i] > key)
-  		{
-			A[i+1] = A[i];
+		key = A[j];
+		i = j - 1;
+		while(i >= 0 && A[i] > key)
+		{
+		    A[i+1] = A[i];
             i = i - 1;
 		}
 		A[i+1] = key;
