c81d86b5e87d814de78379fddc09176dedc12f2c
pp0825pp <905092043@qq.com>
2013-12-17 21:27:20 +0800

    Update 2.3-计数排序.cpp

--- 2.3-计数排序.cpp
+++ 2.3-计数排序.cpp
@@ -4,17 +4,22 @@ int main()
 {
     int A[5] = {4,1,3,4,3}; 
     int B[5], C[4];
-    for(int i=0; i<=3; i++)
+    
+    for(int i = 0; i <= 3; i++)
         C[i] = 0;
-    for(int j=0; j<=4; j++)
+        
+    for(int j = 0; j <= 4; j++)
         C[A[j]-1] = C[A[j]-1] + 1;
-    for(int i=1; i<=3; i++)
+        
+    for(int i = 1; i <= 3; i++)
         C[i] = C[i] + C[i-1];
-    for(int j=4; j>=0; j--)
+        
+    for(int j = 4; j >= 0; j--)
     {
         B[C[A[j]-1]] = A[j];
         C[A[j]-1] = C[A[j]-1] - 1;        
     }
+    
     for(int i=1; i<=5; i++)
         cout<<B[i]<<' ';        
     return 0;
